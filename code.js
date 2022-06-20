57d91d2d01bd0d3b85e5d027dc5888fdc4b12d2e
ishita <ishitanandwani@gmail.com>
2022-06-20 14:05:06 +0530

    Create random function under computerPlay that returns Rock, Paper or Scissor randomly

--- code.js
+++ code.js
@@ -1,1 +1,7 @@
-console.log("Hello World");
+function computerPlay(list) 
+{
+    return list[Math.floor((Math.random()*list.length))];
+}
+  
+  
+  console.log(computerPlay(["Rock", "Paper", "Scissor"]));
