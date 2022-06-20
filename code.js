23d50b03cd06188dc120097147c7b2eb97c2a9fd
ishita <ishitanandwani@gmail.com>
2022-06-20 14:22:13 +0530

    Add code to play single round of the game with player and computer selection

--- code.js
+++ code.js
@@ -2,6 +2,20 @@ function computerPlay(list)
 {
     return list[Math.floor((Math.random()*list.length))];
 }
-  
-  
-  console.log(computerPlay(["Rock", "Paper", "Scissor"]));
+
+let computerSelection = computerPlay(["Rock", "Paper", "Scissor"]);
+
+let playerSelection = prompt("Enter rock or paper or scissor");
+
+if(computerSelection === "Rock" && playerSelection === "Paper")
+console.log("You Win! " + playerSelection + " beats " + computerSelection);
+if(computerSelection === "Rock" && playerSelection === "Scissor")
+console.log("You Lose! " + computerSelection + " beats " + playerSelection);
+if(computerSelection === "Paper" && playerSelection === "Scissor")
+console.log("You Win! " + playerSelection + " beats " + computerSelection);
+if(computerSelection === "Paper" && playerSelection === "Rock")
+console.log("You Lose! " + computerSelection + " beats " + playerSelection);
+if(computerSelection === "Scissor" && playerSelection === "Rock")
+console.log("You Win! " + playerSelection + " beats " + computerSelection);
+if(computerSelection === "Scissor" && playerSelection === "Paper")
+console.log("You Lose! " + computerSelection + " beats " + playerSelection);
